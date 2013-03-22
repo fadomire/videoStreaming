17159f07fc11c7021bb0d2e22d9092e7b34abb29
Fadomire <fadomire@gmail.Com>
2013-03-22 18:39:36 +0100

    initial import

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require("express"),
+    BinaryServer = require('binaryjs').BinaryServer,
+    fs = require('fs');
+  
+var port = process.env.PORT || 8080;
+
+var app = express(), 
+    server = require('http').createServer(app),
+    bs = BinaryServer({server : server});
+
+// Wait for new user connections
+bs.on('connection', function (client){
+  // Define streams we must push to
+  var clientStreams = [];
+  // Incoming stream from a client
+  client.on('stream', function (stream, meta){
+    // Find all connected clients
+    var clients = bs.clients;
+    // Create a stream for each clients
+    for(i in clients){
+      // Dont send data to the broadcaster and dont create a new stream if already exists
+      if(client!=clients[i] && Object.keys(clients[i].streams).length == 0){
+        clientStreams.push(clients[i].createStream({clientId : clients[i].id}));
+      } 
+    }
+    // Push data to clients
+    for(i in clientStreams){
+      stream.pipe(clientStreams[i]);
+    }
+    
+    stream.on('open', function (stream){
+      console.log(stream);
+    });
+    stream.on('end', function (){
+      console.log("stream is received 100% on server")
+      //client.close();
+    });
+  });
+});
+
+server.listen(port);
+
+app.set('view engine', 'ejs');
+app.set("view options", {layout: false});
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+});
+app.get('/', function(req, res){
+  res.render('index', {})
+});
+console.log('Listening on port 8080');
---
+++ public/js/client.js
@@ -0,0 +1,83 @@
+//(function() {
+  window.MediaSource = window.MediaSource || window.WebKitMediaSource;
+  var client = new BinaryClient('ws://192.168.0.14:8080');
+  var URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+  if( ! URL ){
+    URL={};
+  }
+  if( ! URL.createObjectURL) {
+    URL.createObjectURL=function(obj){return obj;}
+  }
+
+  
+    $("#videoSource").change(function (e){
+      var file = e.target.files[0];
+      var fileURL = URL.createObjectURL(file);
+      // Remove old stream video tag
+      if(document.getElementById('streamSource')){
+        document.getElementById('app').removeChild(document.getElementById('streamSource'));
+      }
+      var streamSource = document.createElement('video');
+      streamSource.id = 'streamSource';
+      streamSource.src = fileURL;
+      streamSource.controls = true;
+      streamSource.addEventListener('play', function(){
+        client.send(file, {name: file.name, size: file.size});
+      }, false);
+      document.getElementById('app').appendChild(streamSource);
+    });
+    
+    // Wait for client to connect to server
+    client.on('open', function (stream){
+      // Stream received from server
+      client.on('stream', function (stream){
+        console.log("new stream from server")
+        // Remove old stream video tag
+        if(document.getElementById('streamClient')){
+          document.getElementById('app').removeChild(document.getElementById('streamClient'));
+        }
+        // Create media source
+        var mediaSource = new MediaSource();
+        var streamClient = document.createElement('video');
+        streamClient.id = 'streamClient';
+        streamClient.src = URL.createObjectURL(mediaSource);
+        streamClient.controls = true;
+        streamClient.autoplay = true;
+        document.getElementById('app').appendChild(streamClient);
+        // Wait for media source to be loaded before appending buffer
+        mediaSource.addEventListener('webkitsourceopen', function(e) {
+          // Append buffer
+          mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+          // Wait for streamed data from server
+          stream.on('data', function (data){  
+            console.log("data coming")
+            //console.log(mediaSource)
+            // Append data to client buffer
+            mediaSource.sourceBuffers[0].append(new Uint8Array(data));
+          });
+          stream.on('end', function (){
+            console.log("stream 100% received on client")
+            stream.destroy();
+          });
+          stream.on('drain', function (){
+            console.log('stream drained')
+          });
+        }, false);
+        
+        
+        // if(mediaSource.sourceBuffers.length>0){
+        //   mediaSource.removeSourceBuffer(mediaSource.sourceBuffers[0]);
+        // }
+
+        
+        mediaSource.addEventListener('webkitsourceended', function(e) {
+          console.log('mediaSource readyState: ' + this.readyState);
+        }, false);
+      });
+    });
+    client.on('close', function (){
+      console.log('client closed')
+
+    });
+    
+//})();
