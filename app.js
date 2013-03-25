75cedd9680b004d6e95ae14299e6c98a35e183e5
Fadomire <fadomire@gmail.Com>
2013-03-25 12:39:12 +0100

    differentiate broadcast from watching

--- app.js
+++ app.js
@@ -8,32 +8,35 @@ var app = express(),
     server = require('http').createServer(app),
     bs = BinaryServer({server : server});
 
-// Wait for new user connections
-bs.on('connection', function (client){
+function broadcast(){
   // Define streams we must push to
   var clientStreams = [];
-  // Incoming stream from a client
-  client.on('stream', function (stream, meta){
-    // Find all connected clients
-    var clients = bs.clients;
-    // Create a stream for each clients
-    for(i in clients){
-      // Dont send data to the broadcaster and dont create a new stream if already exists
-      if(client!=clients[i] && Object.keys(clients[i].streams).length == 0){
-        console.log(clients[i].id);
-        clientStreams.push(clients[i].createStream({clientId : clients[i].id}));
-      } 
-    }
-    // Push data to clients
-    for(i in clientStreams){
-      stream.pipe(clientStreams[i]);
-    }
-    stream.on('end', function (){
-      console.log("stream is received 100% on server")
+
+  // Wait for new user connections
+  bs.on('connection', function (client){
+    console.log("Client "+client)
+    // Incoming stream from a client
+    client.on('stream', function (stream, meta){
+      // Find all connected clients
+      var clients = bs.clients;
+      // Create a stream for each clients
+      for(i in clients){
+        // Dont send data to the broadcaster and dont create a new stream if already exists
+        if(client!=clients[i] && Object.keys(clients[i].streams).length == 0){
+          console.log(clients[i].id);
+          clientStreams.push(clients[i].createStream({clientId : clients[i].id}));
+        } 
+      }
+      // Push data to clients
+      for(i in clientStreams){
+        stream.pipe(clientStreams[i]);
+      }
+      stream.on('end', function (){
+        console.log("stream is received 100% on server");
+      });
     });
   });
-});
-
+}
 server.listen(port);
 
 app.set('view engine', 'ejs');
@@ -41,6 +44,10 @@ app.set("view options", {layout: false});
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
+});
+app.get('/broadcast', function(req, res){
+  broadcast();
+  res.render('index', {})
 });
 app.get('/', function(req, res){
   res.render('index', {})
